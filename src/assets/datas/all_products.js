6c5f976dc9c02e86f8cc262d3cbf4f00ec978b75
Nguyễn Anh Duy <duynguyenbt21093@gmail.com>
2025-02-16 20:08:25 +0700

    add data

---
+++ src/assets/datas/all_products.js
@@ -0,0 +1,185 @@
+import p1_img from "./src/assets/images/product-1.png"
+import p2_img from "./src/assets/images/product-2.png"
+import p3_img from "./src/assets/images/product-3.png"
+import p4_img from "./src/assets/images/product-4.png"
+import p5_img from "./src/assets/images/product-5.png"
+import p6_img from "./src/assets/images/product-6.png"
+import p7_img from "./src/assets/images/product-7.png"
+import p8_img from "./src/assets/images/product-8.png"
+import p9_img from "./src/assets/images/product-9.png"
+import p10_img from "./src/assets/images/product-10.png"
+import p11_img from "./src/assets/images/product-11.png"
+import p12_img from "./src/assets/images/product-12.png"
+import p13_img from "./src/assets/images/product-13.png"
+import p14_img from "./src/assets/images/product-14.png"
+import p15_img from "./src/assets/images/product-15.png"
+import p16_img from "./src/assets/images/product-16.png"
+import p17_img from "./src/assets/images/product-17.png"
+import p18_img from "./src/assets/images/product-18.png"
+import p19_img from "./src/assets/images/product-19.png"
+import p20_img from "./src/assets/images/product-20.png"
+
+let all_product = [
+    {
+        id: 1,
+        name: "Áo nhiều hình con mèo",
+        category: "men",
+        image: p1_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 2,
+        name: "Áo có hình ngôi sao",
+        category: "woman",
+        image: p2_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 3,
+        name: "Áo có hình ngôi sao sáng 4 cánh",
+        category: "men",
+        image: p3_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 4,
+        name: "Áo có hình con người",
+        category: "men",
+        image: p4_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 5,
+        name: "Áo có chữ và màu nâu",
+        category: "men",
+        image: p5_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 6,
+        name: "Áo có chữ phá vỡ tệ",
+        category: "men",
+        image: p6_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 7,
+        name: "Áo có hình nhịp tim",
+        category: "men",
+        image: p7_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 8,
+        name: "Áo ôm eo màu đen",
+        category: "woman",
+        image: p8_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 9,
+        name: "Áo ôm eo màu hồng",
+        category: "woman",
+        image: p9_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 10,
+        name: "Áo có hình Rose",
+        category: "men",
+        image: p10_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 11,
+        name: "Áo có hình đám mây màu xi măng",
+        category: "men",
+        image: p11_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 12,
+        name: "Áo có cái kí tự của bộ bài",
+        category: "men",
+        image: p12_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 13,
+        name: "Áo hình cậu bé hút thuốc",
+        category: "men",
+        image: p13_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 14,
+        name: "Áo loang màu có chữ",
+        category: "men",
+        image: p14_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 15,
+        name: "Áo màu đỏ có chữ",
+        category: "men",
+        image: p15_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 16,
+        name: "Áo có hình bộ xương",
+        category: "men",
+        image: p16_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 17,
+        name: "Quần ngắn có chữ",
+        category: "men",
+        image: p17_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 18,
+        name: "Áo ba lỗ có chữ",
+        category: "men",
+        image: p18_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 19,
+        name: "Áo trắng có dòng chữ màu đen",
+        category: "men",
+        image: p19_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 20,
+        name: "Áo hình phi hành gia",
+        category: "men",
+        image: p20_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+
+
+]
