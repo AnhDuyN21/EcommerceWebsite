9622881531714f6c9679d13d8b854125354eab82
Nguyễn Anh Duy <duynguyenbt21093@gmail.com>
2025-02-17 12:32:33 +0700

    add newCollection to homepage ui

---
+++ src/assets/datas/new_collections.js
@@ -0,0 +1,78 @@
+import p24_img from "/images/product-24.png"
+import p21_img from "/images/product-21.png"
+import p22_img from "/images/product-22.png"
+import p23_img from "/images/product-23.png"
+import p25_img from "/images/product-25.png"
+import p26_img from "/images/product-26.png"
+import p27_img from "/images/product-27.png"
+import p28_img from "/images/product-28.png"
+
+
+const new_collections = [
+    {
+        id: 1,
+        name: "Áo kiểu cổ vuông phối ren",
+        category: "woman",
+        image: p21_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 2,
+        name: "Áo hai dây dáng cong phối nút",
+        category: "woman",
+        image: p22_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 3,
+        name: "Áo hai dây trơn phối nút thun gân dáng ôm",
+        category: "men",
+        image: p23_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 4,
+        name: "Áo croptop tay ngắn viền thắt nơ chất thun gân",
+        category: "woman",
+        image: p24_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 5,
+        name: "Áo thun nam trơn cổ V phong cách Hàn Quốc",
+        category: "man",
+        image: p25_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 6,
+        name: "Áo polo nam cổ Tàu áo thun",
+        category: "man",
+        image: p26_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 7,
+        name: "Quần short nam thể thao vải da cá cotton co giãn",
+        category: "man",
+        image: p27_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    },
+    {
+        id: 8,
+        name: "Quần short nam thể thao vải da cá cotton co giãn",
+        category: "man",
+        image: p28_img,
+        new_price: 100.000,
+        old_price: 50.000,
+    }
+];
+
+export default new_collections
